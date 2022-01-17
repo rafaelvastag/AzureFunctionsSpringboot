aaed6620d301a71865e404133d5f5bfba272ba6c
Rafael Vastag <rgvastag@gmail.com>
2022-01-17 19:41:43 -0300

    DIO

--- dio.function/src/main/java/com/vastag/azure/dio/function/dio/function/DioFunction.java
+++ dio.function/src/main/java/com/vastag/azure/dio/function/dio/function/DioFunction.java
@@ -8,9 +8,11 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.vastag.azure.dio.function.dio.function.models.DIO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -25,8 +27,9 @@ public class DioFunction {
 	 * using "curl" command in bash: 1. curl -d "HTTP Body" {your
 	 * host}/api/HttpExample 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
 	 */
-	@FunctionName("GetPoCValues")
-	public HttpResponseMessage run(@HttpTrigger(name = "req", methods = { HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+	@FunctionName("GetDIORegs")
+	public HttpResponseMessage get(@HttpTrigger(name = "req", methods = { HttpMethod.GET }, 
+		authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 		// Parse query parameter
 		String connectionUrl = System.getenv("ConnectionUrl");
@@ -41,15 +44,58 @@ public class DioFunction {
 				Statement statement = connection.createStatement();) {
 
 			// Create and execute a SELECT SQL statement.
-			String selectSql = "SELECT * FROM [dbo].[POC_PARTNER_XP]";
+			String selectSql = "SELECT * FROM [dbo].[POC_DIO]";
 			resultSet = statement.executeQuery(selectSql);
 			// Print results from select statement
 			while (resultSet.next()) {
-				values.add(resultSet.getString("Description"));
+				values.add(resultSet.getString("Name") + " " + resultSet.getString("Description"));
 			}
 			return request.createResponseBuilder(HttpStatus.OK).body(values).build();
 		} catch (Exception e) {
 			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(e.getMessage()).build();
 		}
 	}
+	
+	@FunctionName("CreateDIOReg")
+	public HttpResponseMessage create(@HttpTrigger(name = "req", methods = { HttpMethod.POST }, 
+		authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<DIO>> request,
+			final ExecutionContext context) {
+		// Parse query parameter
+		String connectionUrl = System.getenv("ConnectionUrl");
+		ResultSet resultSet = null;
+		ArrayList<String> values = new ArrayList<>();
+		DIO register = new DIO();
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        
+        if (request.getBody().isPresent()) {
+        	register  = request.getBody().get();
+		}
+	
+		if (null == connectionUrl) {
+			connectionUrl = System.getenv("SQLCONNSTR_ConnectionUrl");
+		}
+
+		try (Connection connection = DriverManager.getConnection(connectionUrl);
+				Statement statement = connection.createStatement();) {
+
+			// Create and execute a SELECT SQL statement.
+			String selectSql = "SELECT * FROM [dbo].[POC_DIO]";
+			
+			PreparedStatement st = connection.prepareStatement("INSERT INTO POC_DIO (Name, Description) VALUES ( ?, ?)");
+			st.setString(1, register.getName());
+			st.setString(2, register. getDescription());
+			st.execute();
+			
+			resultSet = statement.executeQuery(selectSql);
+			// Print results from select statement
+			while (resultSet.next()) {
+				values.add(resultSet.getString("Name") + " " + resultSet.getString("Description"));
+			}
+			return request.createResponseBuilder(HttpStatus.OK).body(values).build();
+		} catch (Exception e) {
+			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(e.getMessage()).build();
+		}
+	}
+	
+	
 }
---
+++ dio.function/src/main/java/com/vastag/azure/dio/function/dio/function/models/DIO.java
@@ -0,0 +1,39 @@
+package com.vastag.azure.dio.function.dio.function.models;
+
+public class DIO {
+	
+	private int id;
+	private String name;
+	private String description;
+	
+	public DIO() {
+		super();
+	}
+	public DIO(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
